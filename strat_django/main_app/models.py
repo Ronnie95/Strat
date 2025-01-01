73ccf8a77035d65dacfecabe64cd2d11cc902369
Ronnie95 <t_daron@yahoo.com>
2025-01-01 15:40:57 -0500

    2nd model configration

--- strat_django/main_app/models.py
+++ strat_django/main_app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Objectives(models.Model):
+class Objective(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     start_date = models.DateField()
@@ -16,3 +16,17 @@ class Objectives(models.Model):
         ("Complete", "Complete"),
     )
     progress = models.CharField(max_length=30, choices=category_Choices, default="not started")
+
+    def __str__(self):
+        return self.title
+
+class KeyResult(models.Model):
+    objective = models.ForeignKey(Objective, on_delete=models.CASCADE, related_name="key_results")
+    description = models.CharField(max_length=250)
+    target_value = models.FloatField()
+    current_value = models.FloatField()
+    deadline = models.DateField()
+
+    def __str__(self):
+        return self.description
+
