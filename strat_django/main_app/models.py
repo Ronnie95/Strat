e0e7a37933b2fac6943002ade432198e44818eba
Ronnie95 <t_daron@yahoo.com>
2025-01-01 15:26:29 -0500

    first model configuration

--- strat_django/main_app/models.py
+++ strat_django/main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Objectives(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    start_date = models.DateField()
+    end_date = models.DateField
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    category_Choices = (
+        ("Not started", "Not started"),
+        ("In Progress", "In Progress"),
+        ("On Hold", "On Hold"),
+        ("Complete", "Complete"),
+    )
+    progress = models.CharField(max_length=30, choices=category_Choices, default="not started")
