8438d2153a7038acb1420b22c28905a071e163b7
Ronnie95 <t_daron@yahoo.com>
2025-01-01 21:37:19 -0500

    updates

--- strat_django/main_app/urls.py
+++ strat_django/main_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 # this like app.use() in express
 urlpatterns = [
-
+    path('objectives/new/', views.ObjectiveCreate.as_view(), name='objective_create')
 ]
--- strat_django/main_app/views.py
+++ strat_django/main_app/views.py
@@ -11,3 +11,47 @@ from django.views.generic import DetailView
 from django.urls import reverse
 
 # Create your views here.
+
+@method_decorator(login_required, name='dispatch')
+class ObjectiveCreate(CreateView):
+    model = Objective
+    fields = ['title', 'description', 'start_date', 'end_date', 'progress']
+    template_name = "objective_create.html"
+    success_url = "/objectives/"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(ObjectiveCreate, self).form_valid(form)
+
+    def get_success_url(self):
+        print(self.kwargs)
+        return reverse('goal_detail', kwargs={'pk': self.object.pk})
+    
+@method_decorator(login_required, name='dispatch')
+class ObjectiveList(TemplateView):
+    template_name = "objectives.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["Goals"] = Goals.objects.all() # Here we are using the model to query the database for us.
+        return context
+    
+
+@method_decorator(login_required, name='dispatch')
+class GoalDetail(DetailView):
+    model = Goals
+    template_name = "goal_detail.html"
+    
+    def get_queryset(self):
+        return Goals.objects.filter(user=self.request.user)
+    
+class GoalUpdate(UpdateView):
+    model = Goals
+    fields = ['goal_name', 'target_amount', 'target_date', 'notes']
+    template_name = "goal_update.html"
+    success_url = "/goal/"
+
+class GoalDelete(DeleteView):
+    model = Goals
+    template_name = "goal_delete.html"
+    success_url = "/goal/"
