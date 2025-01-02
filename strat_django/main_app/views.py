ea09751215471f76052acf2a06ff1f4e29c2aee6
Ronnie95 <t_daron@yahoo.com>
2025-01-01 21:23:26 -0500

    updates

--- strat_django/main_app/views.py
+++ strat_django/main_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse # <- a class to handle sending a type of response
+from django.views.generic.base import TemplateView
+from . models import Objective, KeyResult
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import DetailView
+from django.urls import reverse
 
 # Create your views here.
