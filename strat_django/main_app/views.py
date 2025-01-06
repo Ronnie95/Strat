e86739cf06047ca404668e2596f53ac186b63bd5
Ronnie95 <t_daron@yahoo.com>
2025-01-06 11:46:22 -0500

    view updates

--- strat_django/main_app/views.py
+++ strat_django/main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse # <- a class to handle sending a type of response
 from django.views.generic.base import TemplateView
-from . models import Objective, KeyResult, Swot, SwotItem
+from . models import Objective, KeyResult, Swot, SwotItem, MindMap, Ideas
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -236,3 +236,48 @@ class SwotItemDelete(DeleteView):
     success_url = "/swotitems/"
  ##may have to have the success URL be to objectives due to the one to many relationship
 
+@method_decorator(login_required, name='dispatch')
+class MindMapCreate(CreateView):
+    model = MindMap
+    fields = ['name', 'description']
+    template_name = "mindmap_create.html"
+    success_url = "/mindmaps/"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(MindMapCreate, self).form_valid(form)
+
+    def get_success_url(self):
+        print(self.kwargs)
+        return reverse('mindmap_detail', kwargs={'pk': self.object.pk})
+    
+@method_decorator(login_required, name='dispatch')
+class MindMapList(TemplateView):
+    template_name = "mindmaps.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["MindMap"] = MindMap.objects.all() # Here we are using the model to query the database for us.
+        return context
+    
+
+@method_decorator(login_required, name='dispatch')
+class MindMapDetail(DetailView):
+    model = MindMap
+    template_name = "mindmap_detail.html"
+    context_object_name = 'mindmap'
+
+    
+    def get_queryset(self):
+        return MindMap.objects.filter(user=self.request.user)
+    
+class MindMapUpdate(UpdateView):
+    model = MindMap
+    fields = ['name', 'description']
+    template_name = "mindmap_update.html"
+    success_url = "/mindmaps/"
+
+class MindMapDelete(DeleteView):
+    model = MindMap
+    template_name = "mindmap_delete.html"
+    success_url = "/mindmaps/"
