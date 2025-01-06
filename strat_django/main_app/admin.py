8633176f70b301ff7ca13d63f07c2e79a61a6d9d
Ronnie95 <t_daron@yahoo.com>
2025-01-06 11:39:23 -0500

    admin registry updates

--- strat_django/main_app/admin.py
+++ strat_django/main_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from . models import Objective, KeyResult, Swot, SwotItem
+from . models import Objective, KeyResult, Swot, SwotItem, MindMap, Ideas
 # Register your models here.
 
 admin.site.register(Objective)
 admin.site.register(KeyResult)
 admin.site.register(SwotItem)
 admin.site.register(Swot)
+admin.site.register(MindMap)
+admin.site.register(Ideas)
