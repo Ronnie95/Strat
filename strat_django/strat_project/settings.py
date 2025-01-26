1e6f2a4c76277ceab7fa602674dd3018882f11e2
Ronnie95 <t_daron@yahoo.com>
2025-01-25 21:39:19 -0500

    CSS updates

--- strat_django/strat_project/settings.py
+++ strat_django/strat_project/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_URL = '/static/'
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'main_app/static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
